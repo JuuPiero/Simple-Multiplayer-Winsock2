e205e91854686d6afc414e877afabb934de22d8b
Thanh Hoang <thanhhoang1001@hotmail.com>
2025-05-26 18:24:00 +0700

    clean

--- Client/include/Entry.h
+++ Client/include/Entry.h
@@ -8,24 +8,13 @@ extern Shared<JuuPiero::Application> CreateApplication();
 
 int main(int argc, char *argv[]) {
     JuuPiero::Application::LoadDotenv("./.env");
-    
     const int PORT = atoi(getenv("PORT"));
-
     auto client = JuuPiero::SocketClient::GetInstance();
-
-    try
-    {
-        if (client->Connect(getenv("SERVER_ADDRESS"), PORT)) {
-            std::cout << "Connect to " << getenv("SERVER_ADDRESS") << ":" << PORT << std::endl;
-            auto app = CreateApplication();
-            app->Run();
-        }
+    if (client->Connect(getenv("SERVER_ADDRESS"), PORT)) {
+        std::cout << "Connect to " << getenv("SERVER_ADDRESS") << ":" << PORT << std::endl;
+        auto app = CreateApplication();
+        app->Run();
     }
-    catch(const std::exception& e)
-    {
-        std::cerr << e.what() << '\n';
-    }
-    
     client->Disconnect();
     return 0;
 }
